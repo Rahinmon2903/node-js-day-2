918d442df330e4ce01957108f30ea2ce001b95f1
Rahinmon2903 <rahin2903@gmail.com>
2025-11-26 10:12:23 +0530

    comments added

--- index.js
+++ index.js
@@ -3,20 +3,28 @@ import express from "express"
 
 
 
-
+//configuration
 dotenv.config();
 
+
+//initialization
 const app=express();
 
+
+//port number
 const port=process.env.PORT
 
+//inbuilt middleware
 app.use(express.json());
 
+//listen is used to tell the server in which port it is running
 app.listen(port,()=>{
     console.log("server started at 3000");
     
 })
 
+
+//default route
 app.get("/",(req,res)=>{
      res.status(200).send("default page")
 })
